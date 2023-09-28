dbe51c2be23b501136716d79939a22715d7b03c9
Gavs <poOdrag0n69@gmail.com>
2023-09-28 16:57:51 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+ASP.NET 6 Core application made with C#, Entity Framework Core, and SQL Server.
---
+++ aspnet6_app/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace aspnet6_app.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
