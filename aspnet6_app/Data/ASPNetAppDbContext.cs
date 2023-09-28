6bbe51d2c1b2db582d226bed7f1f756bc8d45e79
Gavs <poOdrag0n69@gmail.com>
2023-09-29 00:43:37 +0800

    feat: Added Model Data

---
+++ aspnet6_app/Data/ASPNetAppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace aspnet6_app.Data
+
+{
+    public class ASPNetAppDbContext : DbContext
+    {
+        public ASPNetAppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<> MyProperty { get; set; }
+    }
+}
