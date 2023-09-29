d4ddcffa6025f3efbfa68186ecdf9124e6323be3
Gavs <poOdrag0n69@gmail.com>
2023-09-29 19:49:33 +0800

    feat: Added Update and Delete Functionality

--- aspnet6_app/Controllers/ProductController.cs
+++ aspnet6_app/Controllers/ProductController.cs
@@ -65,10 +65,85 @@ namespace aspnet6_app.Controllers
                     Price = product.Price,
                     Description = product.Description
                 };
-                return View(viewModel);
+                return await Task.Run( () => View(viewModel));
             }
 
             return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Update(int id)
+        {
+            var product = await aspnetappDbContext.Products.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (product != null)
+            {
+                var viewModel = new ViewProductViewModel()
+                {
+                    Id = product.Id,
+                    Name = product.Name,
+                    Variant = product.Variant,
+                    Qty = product.Qty,
+                    Price = product.Price,
+                    Description = product.Description
+                };
+                return await Task.Run(() => View(viewModel));
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(ViewProductViewModel model)
+        {
+            var product = await aspnetappDbContext.Products.FindAsync(model.Id);
+
+            if (product != null)
+            {
+                product.Name = model.Name;
+                product.Variant = model.Variant;
+                product.Qty = model.Qty;
+                product.Price = model.Price;
+                product.Description = model.Description;
+
+                await aspnetappDbContext.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(long id)
+        {
+            var product = await aspnetappDbContext.Products.FindAsync(id);
+
+            if (product != null)
+            {
+                aspnetappDbContext.Products.Remove(product);
+                await aspnetappDbContext.SaveChangesAsync();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Search(string query)
+        {
+
+            var products = await aspnetappDbContext.Products
+            .Where(p => p.Name.Contains(query))
+            .ToListAsync();
+
+            if (string.IsNullOrEmpty(query))
+            {
+                var all_products = await aspnetappDbContext.Products.ToListAsync();
+
+                return View("Index", all_products);
+            }
+
+            return View("Index", products);
         }
 
     }
